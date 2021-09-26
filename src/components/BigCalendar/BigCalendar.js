2c4fae5c61059df0b4195f6bf31dead6680fae08
Hao <tangsihao1998@gmail.com>
2021-09-26 22:23:43 +0700

    add big calendar

--- src/components/BigCalendar/BigCalendar.js
+++ src/components/BigCalendar/BigCalendar.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import './BigCalendar.scss';
-
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
 
 class BigCalendar extends Component {
   render() {
     return (
       <React.Fragment>
-        
+        <FullCalendar
+          plugins={[ dayGridPlugin ]}
+          initialView="dayGridMonth"
+        />
       </React.Fragment>
     )
   }
